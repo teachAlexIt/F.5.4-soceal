7efbb77885e21fc219acd6e6e77e18e0115bac1e
alekseiteacherit <alekseiteacherit@gmail.com>
2022-04-11 14:24:26 +0300

    завершён раздел диалоги

--- src/components/Diologs/Massages/Massages.js
+++ src/components/Diologs/Massages/Massages.js
@@ -8,6 +8,11 @@ function Massages() {
       <Massage img={Img} name='Alex' text='Test-text'/>
       <Massage img={Img} name='Alex' text='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae officia nobis et architecto similique provident neque incidunt praesentium, minima velit, in voluptatibus doloribus laboriosam optio est soluta culpa dolor quaerat saepe illum, dolorum consectetur facilis? Quas porro quam quia modi, ipsum culpa exercitationem voluptate libero hic animi! Delectus, error corporis?'/>
       <Massage img={Img} name='Alex' text='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae officia nobis et architecto similique provident neque incidunt praesentium, minima velit, in voluptatibus doloribus laboriosam optio est soluta culpa dolor quaerat saepe illum, dolorum consectetur facilis? Quas porro quam quia modi, ips error corporis?'/>
+      <Massage img={Img} name='Alex' text='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae officia nobis et architecto similique provident neque incidunt praesentium, minima velit, in voluptatibus doloribus laboriosam optio est soluta culpa dolor quaerat saepe illum, dolorum consectetur facilis? Quas porro quam quia modi, ips error corporis?'/>
+      <Massage img={Img} name='Alex' text='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae officia nobis et architecto similique provident neque incidunt praesentium, minima velit, in voluptatibus doloribus laboriosam optio est soluta culpa dolor quaerat saepe illum, dolorum consectetur facilis? Quas porro quam quia modi, ips error corporis?'/>
+      <Massage img={Img} name='Alex' text='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae officia nobis et architecto similique provident neque incidunt praesentium, minima velit, in voluptatibus doloribus laboriosam optio est soluta culpa dolor quaerat saepe illum, dolorum consectetur facilis? Quas porro quam quia modi, ips error corporis?'/>
+      <Massage img={Img} name='Alex' text='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae officia nobis et architecto similique provident neque incidunt praesentium, minima velit, in voluptatibus doloribus laboriosam optio est soluta culpa dolor quaerat saepe illum, dolorum consectetur facilis? Quas porro quam quia modi, ips error corporis?'/>
+
     </div>
   )
 }
